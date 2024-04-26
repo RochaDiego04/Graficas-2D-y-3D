7978458f82be078b4354129a186db0f08e8d9516
Diego Rocha <drochaesc@gmail.com>
2024-04-25 22:31:23 -0600

    Proyecto 2: Migrando java Graphics a putPixel y metodos propios

--- src/proyectoDK/DonkeyKongGame.java
+++ src/proyectoDK/DonkeyKongGame.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
 
@@ -20,13 +22,21 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
     private boolean reachedSixthVerticalLine = false;
     private boolean reachedSeventhhLine = false;
 
+    // Pendientes de movimiento de barril
+    private double slopeFirstLine;
+    private double slopeThirdLine;
+    private double slopeFifthLine;
+    private double slopeSeventhLine;
+
     private int BARREL_HEIGHT = 20;
 
     private boolean isRunning;
-    private Thread gameThread;
 
-    private BufferedImage buffer;
-    private Graphics bufferGraphics;
+    private Image fondo;
+    private BufferedImage bufferImage;
+    private Image buffer;
+    private Graphics graPixel;
+
 
     private int playerX;
     private int playerY;
@@ -54,11 +64,12 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
+        setBackground(Color.BLACK);
 
         addKeyListener(this);
 
         buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-        bufferGraphics = buffer.getGraphics();
+
 
         playerX = 20;
         playerY = HEIGHT - 60;
@@ -68,20 +79,78 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
         barrelVisible = false;
 
         isRunning = true;
-        gameThread = new Thread(this);
-        gameThread.start();
+        bufferImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+
+        buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        graPixel = buffer.getGraphics(); // Inicializa graPixel aquí
+
+        calculateSlopes();
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     public void run() {
         while (isRunning) {
-            update();
-            render();
+            logicMoveBarrel();
+            repaint();
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        repaint();
+        if (fondo == null) {
+            fondo = createImage(getWidth(), getHeight());
+            Graphics gfondo = fondo.getGraphics();
+            gfondo.setClip(0, 0, getWidth(), getHeight());
+        }
+        update(graphics);
+    }
+
+    @Override
+    public void update(Graphics graphics) {
+        graphics.setClip(0, 0, getWidth(), getHeight());
+        buffer = createImage(getWidth(), getHeight());
+        graPixel = buffer.getGraphics();
+        graPixel.setClip(0, 0, getWidth(), getHeight());
+        graphics.drawImage(buffer, 0, 0, this);
+    }
+
+    private void calculateSlopes() {
+        // Calcular pendiente para la primera línea
+        int startXFirstLine = 0;
+        int startYFirstLine = 100;
+        int endXFirstLine = 620; // Inicio de la escalera
+        int endYFirstLine = 120;
+        slopeFirstLine = (double) (endYFirstLine - startYFirstLine) / (endXFirstLine - startXFirstLine);
+
+        // Calcular pendiente para la tercera línea
+        int startXThirdLine = 620; // Fin de la escalera
+        int startYThirdLine = 180;
+        int endXThirdLine = 70; // Inicio de la tercera línea
+        int endYThirdLine = 220;
+        slopeThirdLine = (double) (endYThirdLine - startYThirdLine) / (endXThirdLine - startXThirdLine);
+
+        // Calcular pendiente para la quinta línea
+        int startXFifthLine = 250; // Fin de la segunda escalera
+        int startYFifthLine = 300;
+        int endXFifthLine = 950; // Inicio de la quinta línea
+        int endYFifthLine = 350;
+        slopeFifthLine = (double) (endYFifthLine - startYFifthLine) / (endXFifthLine - startXFifthLine);
+
+        // Calcular pendiente para la séptima línea
+        int startXSeventhLine = 650; // Fin de la escalera
+        int startYSeventhLine = 425;
+        int endXSeventhLine = 0; // Inicio de la tercera línea
+        int endYSeventhLine = 465;
+        slopeSeventhLine = (double) (endYSeventhLine - startYSeventhLine) / (endXSeventhLine - startXSeventhLine);
+        System.out.println("1: " + slopeFirstLine + "2: " +slopeThirdLine + "5: " + slopeFifthLine + "7: " + slopeSeventhLine);
     }
 
     private void moveBarrelFirstLine() {
@@ -91,12 +160,11 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
         int endX = 620; // Inicio de la escalera
         int endY = 120;
 
-        double slope = (double) (endY - startY) / (endX - startX);
-        double intercept = startY - slope * startX;
+        double intercept = startY - slopeFirstLine * startX;
 
         barrelX++;
 
-        barrelY = (int) (slope * barrelX + intercept) - BARREL_HEIGHT;
+        barrelY = (int) (slopeFirstLine * barrelX + intercept) - BARREL_HEIGHT;
 
         // Verificar si se alcanzó el final de la primera línea
         if (barrelX >= endX) {
@@ -125,12 +193,11 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
         int endX = 70; // Inicio de la tercera línea
         int endY = 220;
 
-        double slope = (double) (endY - startY) / (endX - startX);
-        double intercept = startY - slope * startX;
+        double intercept = startY - slopeThirdLine * startX;
 
         barrelX--;
 
-        barrelY = (int) (slope * barrelX + intercept) - BARREL_HEIGHT;
+        barrelY = (int) (slopeThirdLine * barrelX + intercept) - BARREL_HEIGHT;
         if (barrelX <= 240) {
             reachedSecondVerticalLine = true;
         }
@@ -157,12 +224,11 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
         int endX = 950; // Inicio de la quinta línea
         int endY = 350;
 
-        double slope = (double) (endY - startY) / (endX - startX);
-        double intercept = startY - slope * startX;
+        double intercept = startY - slopeFifthLine * startX;
 
         barrelX++;
 
-        barrelY = (int) (slope * barrelX + intercept) - BARREL_HEIGHT;
+        barrelY = (int) (slopeFifthLine * barrelX + intercept) - BARREL_HEIGHT;
         if (barrelX >= 640) {
             reachedSixthVerticalLine = true;
         }
@@ -189,12 +255,11 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
         int endX = 0; // Inicio de la tercera línea
         int endY = 465;
 
-        double slope = (double) (endY - startY) / (endX - startX);
-        double intercept = startY - slope * startX;
+        double intercept = startY - slopeSeventhLine * startX;
 
         barrelX--;
 
-        barrelY = (int) (slope * barrelX + intercept) - BARREL_HEIGHT;
+        barrelY = (int) (slopeSeventhLine * barrelX + intercept) - BARREL_HEIGHT;
         if (barrelX <= 240) {
             reachedSecondVerticalLine = true;
         }
@@ -248,7 +313,23 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
         }
     }
 
-    private void update() {
+    private void resetBarrel() {
+        barrelX = 50;
+        barrelY = 100;
+        barrelVisible = true;
+
+        reachedVerticalLine = false;
+        reachedThirdLine = false;
+        reachedSecondVerticalLine = false;
+        reachedFifthLine = false;
+        reachedSixthVerticalLine = false;
+        reachedSeventhhLine = false;
+
+        barrelState = BarrelState.FIRST_LINE;
+    }
+
+
+    public void logicMoveBarrel() {
         // Mueve el barril hacia abajo si está visible
         if (barrelVisible) {
             moveBarrel();
@@ -258,53 +339,47 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
                 JOptionPane.showMessageDialog(this, "¡Has sido golpeado por un barril! Juego terminado.");
                 System.exit(0);
             }
-            // Si el barril llega al suelo, desaparece
-            if (barrelY >= HEIGHT) {
+            // Si el barril llega al suelo o sale de los limites en X, desaparece
+            if (barrelY >= HEIGHT || barrelX < 0) {
                 barrelVisible = false;
             }
         } else {
-            // Genera un nuevo barril después de un cierto intervalo de tiempo
+            // Genera un nuevo barril un tiempo aleatorio despues de que el primero desaparezca
             if (Math.random() < 0.01) {
-                barrelX = 50;
-                barrelY = 50;
-                barrelVisible = true;
+                resetBarrel();
             }
         }
     }
 
-    private void render() {
-        // Dibuja todos los elementos en el buffer
-        bufferGraphics.setColor(Color.BLACK);
-        bufferGraphics.fillRect(0, 0, WIDTH, HEIGHT);
+    @Override
+    public void repaint() {
+        // Llena el buffer con el color negro
+        graPixel.setColor(Color.BLACK);
+        graPixel.fillRect(0, 0, WIDTH, HEIGHT);
 
         // Dibuja al jugador
-        bufferGraphics.setColor(Color.WHITE);
-        bufferGraphics.fillRect(playerX, playerY, 20, 20);
+        drawRectangle(playerX, playerY, playerX + 20, playerY + 20, Color.white);
 
         // Dibuja escenario
-        bufferGraphics.setColor(Color.orange);
-        bufferGraphics.drawLine(0, 100, 700, 120);
-        bufferGraphics.drawLine(800, 170, 70, 220);
-        bufferGraphics.drawLine(0, 280, 700, 330);
-        bufferGraphics.drawLine(800, 415, 0, 465);
+        drawBresenhamLine(0, 700, 100, 120, Color.orange);
+        drawBresenhamLine(800, 70, 170, 220, Color.orange);
+        drawBresenhamLine(0, 700, 280, 330, Color.orange);
+        drawBresenhamLine(800, 0, 415, 465, Color.orange);
 
         // Dibuja escalera corta 1
-        bufferGraphics.setColor(Color.BLUE);
-        bufferGraphics.drawLine(630, 120, 630, 180);
+        drawBresenhamLine(630, 630, 120, 180, Color.blue);
 
         // Dibuja escalera 2
-        bufferGraphics.setColor(Color.BLUE);
-        bufferGraphics.drawLine(250, 210, 250, 300);
+        drawBresenhamLine(250, 250, 210, 300, Color.blue);
 
         // Dibuja escalera 3
-        bufferGraphics.setColor(Color.BLUE);
-        bufferGraphics.drawLine(650, 330, 650, 420);
+        drawBresenhamLine(650, 650, 330, 420, Color.blue);
 
 
         // Dibuja el barril si está visible
         if (barrelVisible) {
-            bufferGraphics.setColor(Color.RED);
-            bufferGraphics.fillRect(barrelX, barrelY, 20, BARREL_HEIGHT);
+            //bufferGraphics.fillRect(barrelX, barrelY, 20, BARREL_HEIGHT);
+            drawRectangle(barrelX, barrelY, barrelX + 20, barrelY + 20, Color.red);
         }
 
         // Dibuja el buffer en la pantalla
@@ -314,6 +389,7 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
             g.dispose();
         }
     }
+
 
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
@@ -333,6 +409,76 @@ public class DonkeyKongGame extends JFrame implements Runnable, KeyListener {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new DonkeyKongGame().setVisible(true));
+        DonkeyKongGame donkeyKongGame = new DonkeyKongGame();
+        Thread gameThread = new Thread(donkeyKongGame);
+        gameThread.start();
     }
+
+
+    // METODOS DE DIBUJO
+    private void putPixel(int x, int y, Color color) {
+        bufferImage.setRGB(0, 0, color.getRGB());
+        graPixel.drawImage(bufferImage, x, y, this);
+    }
+    public void drawBresenhamLine(int x0, int x1, int y0, int y1, Color color) {
+        int dx = Math.abs(x1 - x0);
+        int dy = Math.abs(y1 - y0);
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx - dy;
+        int err2;
+
+        while (x0 != x1 || y0 != y1) {
+            putPixel(x0, y0, color);
+            err2 = 2 * err;
+            if (err2 > -dy) {
+                err -= dy;
+                x0 += sx;
+            }
+            if (err2 < dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+    public void drawRectangle(int x0, int y0, int x1, int y1, Color color) {
+        // If x0 has a greater value, exchange values with x1
+        if (x0 > x1) {
+            int temp = x0;
+            x0 = x1;
+            x1 = temp;
+        }
+        if (y0 > y1) {
+            int temp = y0;
+            y0 = y1;
+            y1 = temp;
+        }
+
+        drawBresenhamLine(x0, x1, y0, y0, color);
+        drawBresenhamLine(x0, x1, y1, y1, color);
+        drawBresenhamLine(x0, x0, y0, y1, color);
+        drawBresenhamLine(x1, x1, y0, y1, color);
+    }
+
+    /*public void floodFill(int x, int y, Color targetColor, Color fillColor) {
+        Queue<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(x, y));
+
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            int px = point.x;
+            int py = point.y;
+
+            if (px >= 0 && px < getWidth() && py >= 0 && py < getHeight() &&
+                    bufferImage.getRGB(px, py) == targetColor.getRGB()) {
+                putPixel(px, py, fillColor);
+
+                // Revisar los vecinos
+                queue.add(new Point(px + 1, py));
+                queue.add(new Point(px - 1, py));
+                queue.add(new Point(px, py + 1));
+                queue.add(new Point(px, py - 1));
+            }
+        }
+    }*/
 }
